7e972566734ae1c08007070e093867478a86ef52
Lingchen Zhu <zhulingchen@gmail.com>
2020-10-22 15:51:56 -0400

    update README.md to include usage

--- README.md
+++ README.md
@@ -7,6 +7,18 @@
 * opencv-python
 * [torchsummary](https://github.com/sksq96/pytorch-summary)
 
+## Usage
+
+main.py [-h] -c CONFIG -d DATASET [-g GENERATE] [-s SAVEFILE] {train,test}
+
+| Arguments     	                                  | Description                         |
+| :---          				                      | :---                                |
+| -h, --help                                          | show this help message and exit     |
+| -c CONFIG, --config CONFIG                          | config file                 	    |
+| -d DATASET, --dataset DATASET                       | dataset name                        |
+| -i [INPUT [INPUT ...]], --input [INPUT [INPUT ...]] | input source image (test mode only) |
+| {train,test}                                        | work mode                           |
+
 ## Reference
 
 * [Website](https://phillipi.github.io/pix2pix/)
--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from model import *
 
 if __name__ == '__main__':
     # define input arguments
-    # train example: train -d facades -c config.yaml
-    # test example: test -d facades -c config.yaml -i datasets/facades/test/1_src.jpg datasets/facades/test/2_src.jpg
+    # train example: train -c config.yaml -d facades
+    # test example: test -c config.yaml -d facades -i datasets/facades/test/1_src.jpg datasets/facades/test/2_src.jpg
     parser = argparse.ArgumentParser()
     parser.add_argument('mode', type=str.lower, choices=['train', 'test'], help='work mode')
     parser.add_argument('-c', '--config', required=True, type=str, help="config file")
-    parser.add_argument('-d', '--dataset', required=True, type=str, help="dataset name (train mode only)")
+    parser.add_argument('-d', '--dataset', required=True, type=str, help="dataset name")
     parser.add_argument('-i', '--input', type=str, nargs='*', help="input source image (test mode only)")
     args = parser.parse_args()
     if (args.mode == 'test') and ('input' not in args):
